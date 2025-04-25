1ead9bc022c6d9f5e12832ffa5bef8e3bea3584d
ilya-noize <shumakoff.ilya@yandex.ru>
2025-04-25 21:42:41 +0300

    start spring

---
+++ src/main/java/com/example/logisight/LogisightApplication.java
@@ -0,0 +1,13 @@
+package com.example.logisight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/logisight/LogisightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logisight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
